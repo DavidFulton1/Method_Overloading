6baca2b24d2e8cbf18cea3f03f4682b09f8a67c8
DavidFulton1 <fultondavids@gmail.com>
2022-07-11 13:44:58 -0500

    second commit

--- Method_Overloading/Methods.cs
+++ Method_Overloading/Methods.cs
@@ -8,5 +8,38 @@ namespace Method_Overloading
 {
     internal class Methods
     {
+        public static int Add(int x, int y)
+        {
+            int answer = x + y;
+            return answer;
+        }
+        public static decimal Add(decimal x, decimal y)
+        {
+            decimal answer = x + y;
+            return answer;
+        }
+        public static string? Add(int x, int y, bool m)
+        {
+            int sum = x + y;
+            if (m == true && sum > 1)
+            {
+                string answer = $"{sum} dollars";
+                return answer;
+            }
+            else if (m == true && sum < 2)
+            {
+                string answer = $"{sum} dollar";
+                return answer;
+            }
+            else return sum.ToString();
+        }
     }
 }
+
+
+
+
+
+
+
+
